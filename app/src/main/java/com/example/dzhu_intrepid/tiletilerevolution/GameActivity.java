228b49193d92a7e38910e132d7d8e0f730859844
David Zhu <hdavidzhu@gmail.com>
2016-01-04 17:06:54 -0500

    ADD: Fragment to the game board.

--- app/src/main/java/com/example/dzhu_intrepid/tiletilerevolution/GameActivity.java
+++ app/src/main/java/com/example/dzhu_intrepid/tiletilerevolution/GameActivity.java
@@ -1,7 +1,10 @@
 package com.example.dzhu_intrepid.tiletilerevolution;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class GameActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        if (savedInstanceState == null) {
+            Fragment gameBoardFragment = GameBoardFragment.newInstance();
+            FragmentManager fragmentManager = getFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.add(R.id.game_board_container, gameBoardFragment).commit();
+        }
     }
 }
